7048b9eb7ef02aa0241b3d7d4731f048aff0a845
Dimitris Kokkonis <kokkonisd@gmail.com>
2023-02-22 17:29:53 +0100

    Migrate to using pyproject.toml

--- flores/__init__.py
+++ flores/__init__.py
@@ -1,9 +1,4 @@
 """Definitions for the package-level information for Flores."""
 
-name = "flores"
-description = "Yet another static site generator."
 __version_info__ = ("0", "1", "3", "dev")
 __version__ = ".".join(__version_info__)
-__author_name__ = "Dimitri Kokkonis"
-__author_email__ = "kokkonisd@gmail.com"
-__author__ = f"{__author_name__} ({__author_email__})"
