fe8c204da2a6198cee7b14546cdabc99403f443e
Dimitris Kokkonis <kokkonisd@gmail.com>
2023-02-26 16:37:22 +0100

    Bump version in dev

--- flores/__init__.py
+++ flores/__init__.py
@@ -1,4 +1,4 @@
 """Definitions for the package-level information for Flores."""
 
-__version_info__ = ("0", "2", "0", "dev")
+__version_info__ = ("0", "2", "1", "dev")
 __version__ = ".".join(__version_info__)
